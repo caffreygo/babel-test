a7326838083216dbf5973f44cd53cd63006957c6
Jinrui Chen <jinrui@kooboo.cn>
2022-12-06 10:47:30 +0800

    feat: insert parameters plugin

---
+++ console/plugin.js
@@ -0,0 +1,39 @@
+const generate = require("@babel/generator").default;
+const targetCalleeName = ["log", "info", "error", "debug"].map(
+  (item) => `console.${item}`
+);
+
+
+// 作为插件用的时候，不需要自己调用 parse、traverse、generate，通用流程 babel 会做
+// 我们只需要提供一个 visitor 函数，在这个函数内完成转换功能就行了 (api, options) => ({})
+module.exports = function ({ types, template }) {
+  return {
+    visitor: {
+      CallExpression(path, state) {
+        if (path.node.isNew) {
+          return;
+        }
+
+        const calleeName = generate(path.node.callee).code;
+
+        if (targetCalleeName.includes(calleeName)) {
+          const { line, column } = path.node.loc.start;
+
+          const newNode = template.expression(
+            `console.log("${
+              state.filename || "unkown filename"
+            }: (${line}, ${column})")`
+          )();
+          newNode.isNew = true;
+
+          if (path.findParent((path) => path.isJSXElement())) {
+            path.replaceWith(types.arrayExpression([newNode, path.node]));
+            path.skip();
+          } else {
+            path.insertBefore(newNode);
+          }
+        }
+      },
+    },
+  };
+};
