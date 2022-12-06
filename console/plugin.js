39db8fd4cd92fc6aadb37cd3182aa07e785eee24
Jinrui Chen <jinrui@kooboo.cn>
2022-12-06 11:13:08 +0800

    feat: replace generate by path

--- console/plugin.js
+++ console/plugin.js
@@ -1,8 +1,6 @@
-const generate = require("@babel/generator").default;
 const targetCalleeName = ["log", "info", "error", "debug"].map(
   (item) => `console.${item}`
 );
-
 
 // 作为插件用的时候，不需要自己调用 parse、traverse、generate，通用流程 babel 会做
 // 我们只需要提供一个 visitor 函数，在这个函数内完成转换功能就行了 (api, options) => ({})
@@ -13,9 +11,7 @@ module.exports = function ({ types, template }) {
         if (path.node.isNew) {
           return;
         }
-
-        const calleeName = generate(path.node.callee).code;
-
+        const calleeName = path.get("callee").toString();
         if (targetCalleeName.includes(calleeName)) {
           const { line, column } = path.node.loc.start;
 
